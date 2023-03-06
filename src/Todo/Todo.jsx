83bf858ed60180a7a938313a917ea8f37e3672c1
Yogesh saini <yogesh.sain@nanoheal.com>
2023-03-06 18:38:59 +0530

    added search and filter functionalities

--- src/Todo/Todo.jsx
+++ src/Todo/Todo.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import "./Todo.css";
 import {
  
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
   TextField,
 } from "@mui/material";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
@@ -13,6 +17,10 @@ import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import NumberBox from "./SubTodo/NumberBox";
 import Loader from "./SubTodo/Loaders/Loader";
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Todo = () => {
 
@@ -22,6 +30,9 @@ const Todo = () => {
   const [completed, setCompleted] = useState(null);
   const [Loading, setLoading] = useState(false);
   const[text,setText]=useState("")
+  const [userFixedData, setUserFixedData] = useState(null);
+  const [Dropdowndata, setDropdowndata] = useState(null);
+
 
   useEffect(() => {
     //Here we are fetching full List of tasks as soon as page loads
@@ -32,12 +43,14 @@ const Todo = () => {
     setLoading(false);
 
     let myTasks = await fetchApi("GET", `https://dark-rose-monkey-coat.cyclic.app/TodoApp/task`);
- 
+    setUserFixedData(myTasks.Data)
     setAllTask(myTasks.Data);
+    setDropdowndata(myTasks.Data);
+   
     if(myTasks.message)
     {
-      setText(`It's time to achieve your Goals, Let's start with your first
-      Task here...`)
+      setText(`Oops :/ There is no task here, Let's start with your first
+      Task ...`)
     }
    
     let CompletedCount = 0;
@@ -114,13 +127,114 @@ const Todo = () => {
    
   };
 
+  function searchJson(jsonData, searchTerm) {
+    let results = [];
+
+    for (let i = 0; i < jsonData.length; i++) {
+      let object = jsonData[i];
+      console.log(object)
+      for (let property in object) {
+        if (object.hasOwnProperty(property)) {
+          if (
+            typeof object[property] === "string" &&
+            object[property].includes(searchTerm)
+          ) {
+            console.log(object)
+            results.push(jsonData[i]);
+            console.log(results)
+            break;
+          }
+        }
+      }
+    }
+    // console.log("reached")
+    // console.log(usersdata)
+    // console.log(results);
+    setAllTask(results);
+  }
+
+  const filter=async (allTask,Myvalue)=>
+  {
+    let filteredData;
+
+    if(Myvalue==="completed")
+    {
+      filteredData= await allTask.filter((data)=>
+      {
+         return data.completed===true;
+          setAllTask(filteredData);
+        
+  
+  
+      })
+      setAllTask(filteredData);
+
+    }
+    if(Myvalue==="uncompleted")
+    {
+      filteredData= await allTask.filter((data)=>
+      {
+         return data.completed===false;
+  
+  
+      })
+      setAllTask(filteredData);
+
+    }
+    if(Myvalue==="all")
+    {
+      setAllTask(allTask);
+
+    }
+   
+
+    return
+
+  
+
+  }
+
   return (
     <>
     {/* showing loader if data has not came else not showing the loader  */}
       {Loading ? (
         <Loader />
       ) : (
+        
         <div className="todo">
+        <Paper
+      component="form"
+      className="Search-Paper"
+    >
+      <InputBase
+        className='Search-InputBase'
+        placeholder="Search..."
+        onChange={(e) => {
+                e.preventDefault();
+                //  console.log( searchJson(userFixedData, e.target.value));
+                console.log(e.target.value);
+                searchJson(userFixedData, e.target.value);
+              }}
+        inputProps={{ 'aria-label': 'search...' }}
+      />
+      <IconButton type="button" className='Search-Icon' aria-label="search">
+        <SearchIcon className='Search-Icon' />
+      </IconButton>
+    </Paper>
+    <FormControl fullWidth className="mx-1" style={{scale:"0.7",width:"50%",}}>
+  <InputLabel id="demo-simple-select-label">Filter by</InputLabel>
+  <Select
+    labelId="demo-simple-select-label"
+    id="demo-simple-select"
+    // value="filter by"
+    label="Filter by"
+   
+  >
+    <MenuItem onClick={()=>{filter(Dropdowndata,"completed")}} >completed</MenuItem>
+    <MenuItem onClick={()=>{filter(Dropdowndata,"uncompleted")}} >uncompleted</MenuItem>
+    <MenuItem onClick={()=>{filter(Dropdowndata,"all")}} >All</MenuItem>
+  </Select>
+</FormControl>
           <div className="innerdiv" style={{ color: "#6256c0" }}>
             <div style={{ color: "#0092cd" }}>to</div>do
           </div>
@@ -153,6 +267,7 @@ const Todo = () => {
             </div>
           </div>
           <div className="innerthirddiv">
+         
             <div className="createTask my-2">
               <div style={{ color: "#0092cd", display: "flex" }}>
                 Created tasks{" "}
@@ -181,6 +296,7 @@ const Todo = () => {
             </div>
             <div className="Dynamictask">
             {/* showing data scatterd by map function  */}
+            {console.log(allTask)}
               {allTask && allTask.length > 0 ? (
                 allTask.map((data, index) => {
                   return (
